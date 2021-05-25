87ce6004875b4921221348173db5d83c175d4012
Jeremy Charles Kent <jckent72@icloud.com>
2021-05-25 08:33:21 -0400

    my first push

--- trace/omniTrace.py
+++ trace/omniTrace.py
@@ -1,1 +1,8 @@
+import os, re, subprocess
 
+TRACE = ['8.8.8.8 192.168.74.2']
+# TRACE = os.environ['TRACE']
+for addressPair in TRACE:
+    destIP, testIP = addressPair.split(' ')
+    tracerouteOut = subprocess.run('traceroute', destIP, stdout=subprocess.PIPE)
+    print(tracerouteOut.stdout)
