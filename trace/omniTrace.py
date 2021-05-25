166b52620b89b1d97e6b34a8b73156fc32dd58fe
Jeremy Charles Kent <jckent72@icloud.com>
2021-05-25 12:38:59 -0400

    my second push

--- trace/omniTrace.py
+++ trace/omniTrace.py
@@ -1,8 +1,12 @@
-import os, re, subprocess
+import os
 
 TRACE = ['8.8.8.8 192.168.74.2']
 # TRACE = os.environ['TRACE']
+traceDir = os.listdir('/home/pi/OMNI/trace')
+if 'tmp' not in traceDir:
+    os.system('mkdir /home/pi/OMNI/trace/tmp')
+tracerouteOut = open('/home/pi/OMNI/trace/tmp/tracerouteOut.txt', 'w')
+tracerouteOut.close()
 for addressPair in TRACE:
     destIP, testIP = addressPair.split(' ')
-    tracerouteOut = subprocess.run('traceroute', destIP, stdout=subprocess.PIPE)
-    print(tracerouteOut.stdout)
+    os.system('traceroute {} >> /home/pi/OMNI/trace/tmp/tracerouteOut.txt'.format(destIP))
