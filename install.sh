5d11fa53dd73b3e62cc39ba708200b4c7fcfe185
Matt Wagner <mwagner@hackmypi.com>
2021-05-30 14:57:55 -0400

    added reboot email and install.sh runs at reboot.

--- install.sh
+++ install.sh
@@ -4,9 +4,14 @@
 #overclock to 2ghz
 #change hostname
 
+echo "applying overclock"
 sudo python3 /home/pi/OMNI/setup/oc.py
 sudo python3 /home/pi/OMNI/setup/hostname.py
+echo "updating hostname"
+echo "adding OMNI to system path"
 sudo python3 /home/pi/OMNI/setup/omniToPath.py
+echo "adding reboot email to cron"
+sudo crontab -l | { cat; echo "@reboot sleep 15 && sudo python3 /home/pi/OMNI/smtp/rebootEmail.py &"; } | sudo crontab -
 
 
 read -p 'Press any key to reboot to apply changes'
